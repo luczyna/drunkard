f65c0aa07877da3e520ef99ae6cdd643d69f4af6
Ana Lu <luczynaki@gmail.com>
2015-08-14 01:24:42 -0500

    werk on scoring performance

--- src/app/test/test.js
+++ src/app/test/test.js
@@ -36,6 +36,7 @@
             show: false
         };
         tvm.expletive = '';
+        tvm.endOfGame = false;
 
         // stages of the test
         //   0 not started      1 primed
@@ -43,13 +44,14 @@
         //   4 change rule      5 finish and present score
         tvm.stage = 0;
         tvm.ready = false;
-        tvm.count = 5;
+        tvm.count = 3;
 
         tvm.canvasInteraction = canvasInteraction;
         tvm.primeTest = primeTest;
         tvm.startTest = startTest;
         tvm.addRule = addRule;
-
+        tvm.endTest = endTest;
+        tvm.testAgain = testAgain;
 
         init();
 
@@ -93,12 +95,25 @@
         }
 
         function addRule() {}
+
+        function endTest() {
+            // show a message
+            // wait for click
+            // show score 
+            // allow user to go back
+            tvm.message.main = 'Now we will judge you';
+            tvm.message.context = null;
+
+            $timeout(function() {
+            tvm.message.show = true;
+                showResults();
+            }, 1000);
+        }
         
         function canvasInteraction() {
             if (tvm.stage === 1 && tvm.ready) {
                 erasingEntry(function() {
                     // we can go to the next stage
-                    exam.updateLastEntry(entry);
                     tvm.message.show = false;
                     tvm.stage++;
                     tvm.startTest();
@@ -110,6 +125,11 @@
                         testProblem();
                     } else {
                         // end of test round 1... now add rule?
+                        // TODO think of real good rules
+                        // and improve upon the spawning of entries
+                        // right now, we'll just finish the test
+                        tvm.stage = 5;
+                        tvm.endTest();
                     }
                 });
             } else if (tvm.stage === 3 && tvm.ready) {}
@@ -127,6 +147,7 @@
 
             canvasing.eraseBlip().then(
             function() {
+                exam.updateLastEntry(entry);
                 if (callback) {
                     callback();
                 }
@@ -135,10 +156,67 @@
                 console.log('error in canvas erasing');
             });
         }
+
+        function showResults() {
+            var results = exam.scoreExam();
+            tvm.message.main = 'You are ' + results.what;
+            // tvm.message.context = null;
+            if (results.what === 'drunk') {
+                tvm.message.context = 'Gud jahb';
+            } else if (results.what === 'tipsy') {
+                tvm.message.context = 'Keep it up';
+            } else {
+                tvm.message.context = 'You may keep drinking';
+            }
+            tvm.message.show = true;
+            
+            $timeout(function() {
+                tvm.endOfGame = true;
+            }, 1000);
+        }
+
+        function testAgain() {
+            exam.clearEntries();
+            tvm.stage = 0;
+            tvm.endOfGame = false;
+
+            $timeout(function() {
+                init();
+            }, 1000);
+        }
     }
 
     /* @ngInject */
     function messageEntryAnimation() {
+        var hideClass = 'ng-hide';
+        var message = {
+            beforeAddClass: hideMessage,
+            removeClass: showMessage
+        };
+
+        return message;
+
+        ///
+
+        function hideMessage(element, className, done) {
+            if (className !== hideClass) {
+                return;
+            }
+
+            element.fadeOut(300, done);
+        }
+
+        function showMessage(element, className, done) {
+            if (className !== hideClass) {
+                return;
+            }
+            console.log('showing message');
+            element.hide().fadeIn(300, done);
+        }
+    }
+
+    /* @ngInject */
+    function playAgainAnimation() {
         var hideClass = 'ng-hide';
         var message = {
             beforeAddClass: hideMessage,
--- src/common/services/exam.js
+++ src/common/services/exam.js
@@ -19,7 +19,6 @@
             updateLastEntry: updateLastEntry,
             getEntries: getEntries,
             clearEntries: clearEntries,
-            scoreEntry: scoreEntry,
             scoreExam: scoreExam
         };
 
@@ -48,8 +47,77 @@
             entries.length = 0;
         }
         
-        function scoreEntry() {}
-        function scoreExam() {}
+        function scoreExam() {
+            // what is our maximum score?
+            var max = entries.length * 10;
+
+            // of each entry, what is their score?
+            // start off with benefit of doubt
+            var score = max;
+            for (var i = 0; i < entries.length; i++) {
+                var entry = entries[i];
+                var mark = scoreEntry(entry);
+                score -= mark;
+            }
+
+            var result = {
+                what: null,
+                score: score / max
+            };
+            if (score < max / 2) {
+                result.what = 'drunk';
+            } else if (score < max * 0.75) {
+                result.what = 'tipsy';
+            } else {
+                result.what = 'good enough';
+            }
+
+            return result;
+        }
+
+        function scoreEntry(entry) {
+            var mark;
+
+            // how far away from the center
+            // it's bad if it's too much farther than the radius
+            var a, b, c, distance;
+            a = Math.abs(entry.position.x - entry.pinpoint.x);
+            b = Math.abs(entry.position.y - entry.pinpoint.y);
+            c = Math.pow(a, 2) + Math.pow(b, 2);
+            distance = Math.sqrt(c);
+
+            if (distance < entry.radius / 2) {
+                mark = 0;
+            } if (distance < entry.radius * 1.5) {
+                mark = 2;
+            } else {
+                // damn, you suck
+                mark = 4;
+            }
+
+            // how long did it take to finish
+            var time, seconds, moreTrouble;
+            time = Date.parse(entry.answered) - Date.parse(entry.created);
+            seconds = time / 1000;
+            if (seconds < 0.3) {
+                moreTrouble = 0;
+            } else if (seconds < 0.6) {
+                moreTrouble = 3;
+            } else {
+                moreTrouble = 4;
+            }
+            mark += moreTrouble;
+
+            // if this look a lot of time 
+            // AND
+            // you couldn't hit the mark...
+            if (seconds > 0.6 && distance > entry.radius * 2) {
+                // there is no question! RIGHT!?!
+                mark = 10;
+            }
+
+            return mark;
+        }
 
 
 
